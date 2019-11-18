54eb98a3f2f1680f3bebf19f903a9892592678b2
TiboSarl <thibaud.sarlat31@gmail.com>
2019-11-18 15:49:33 +0100

    feat: Test_Arbre_Binaire.adb

--- livrables/Test_Arbre_Binaire.adb
+++ livrables/Test_Arbre_Binaire.adb
@@ -7,4 +7,21 @@ procedure T_Arbre_Binaire is
    package Arbre_BinaireI is
    new Arbre_Binaire (T_Identifiant => Integer);
 
-   procedure Test_Initialiser( Abr : out T_AB
+   procedure Test_Initialiser is
+   Arbre : T_Arbre_Binaire;
+   begin
+   Initialiser(Arbre,1);
+   pragma assert(Est_Vide(Arbre));
+   end Test_Initialiser;
+
+   procedure Test_Ajouter is
+   Arbre : T_Arbre_Binaire;
+   begin
+   Initialiser(Arbre);
+   Ajouter(Arbre, 1);
+   pragma assert(Est_Present(Arbre, 1));
+   Detruire(Arbre);
+   end Test_Ajouter;
+
+   procedure Nombre_de_
+
