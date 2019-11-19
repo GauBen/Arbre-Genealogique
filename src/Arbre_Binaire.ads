f7e2b11e06827b3b12726be1e01de1583b402c80
TiboSarl <thibaud.sarlat31@gmail.com>
2019-11-19 10:23:16 +0100

    fix: improved spec

--- src/Arbre_Binaire.ads
+++ src/Arbre_Binaire.ads
@@ -14,31 +14,69 @@ package Arbre_Binaire is
         Identifiant_Absent_Exception : Exception;
 
         -- Initisalier un Arbre Binaire abr. L'Arbre Binaire est vide.
-        procedure Initialiser (Abr : out T_Arbre_Binaire;ID : in T_Identifiant)
+        -- Paramètres :
+        --             Arbre : out T_Arbre_Binaire   L'arbre que l'on veut créer
+        --             ID    : in T_Identifiant      L'identifiant de la racine
+        -- Exception : Aucune
+        -- Assure :
+        --        Arbre créé avec pour racine l'identifiant donné.
+        procedure Initialiser (Arbre : out T_Arbre_Binaire;ID : in T_Identifiant) with
+            Post => Est_Present(Arbre,ID);
 
-        -- Ajouter un noeud
-        -- Exception : Identifiant_Present_Exception si L'identifiant est déjà présent dans l'arbre.
-        procedure Ajouter(Abr : in out T_Arbre_Binaire;ID_racine : in T_Identifiant; ID_feuille : in T_Identifiant);
+        -- Ajouter un noeud à une racine
+        -- Paramètres :
+        --              Arbre : in out T_Arbre_Binaire  L'arbre que 'l'on veut modifier
+        --              ID_Racine : in T_Identifiant  L'identifiant de la racine à laquelle on veut rajouter une feuille
+        --              ID_feuille : in T_Identifiant  L'identifiant de la feuille que l'on veut rajouter
+        -- Exception : Identifiant_Present_Exception si L'identifiant de la feuille rajouté est déjà présent dans l'arbre.
+        --             Identifiant_Absent_Exception si l'identifiant de la racine n'est pas présent dans l'arbre.
+        -- Assure :
+        --        Dans les bonnes conditions, la feuille est rajouté à l'arbre
+        procedure Ajouter(Arbre : in out T_Arbre_Binaire;ID_racine : in T_Identifiant; ID_feuille : in T_Identifiant) with
+            Post => Est_Present(Arbre : in T_Arbre, ID_feuille);
 
         -- Donner le nombre de successeurs d'un noeud
+        -- Paramètres :
+        --              Arbre : in T_Arbre_Binaire  L'arbre sur lequel nous voulons l'information
+        --              ID : in T_Identifiant       Le noeud de départ
         -- Exception : Identifiant_Absent_Exception si l'identifiant n'est pas présent dans l'arbre.
-        function Nombre_de_successeurs(Abr : in T_Arbre_Binaire; ID : in T_Identifiant) return Integer;
+        -- Assure :
+        --        Nombre_de_successeurs(Arbre,Id) = nombre de feuilles sous le noeud
+        function Nombre_de_successeurs(Arbre : in T_Arbre_Binaire; ID : in T_Identifiant) return Integer;
 
-        -- Obtenir les Identifiants des successeur d'un noeud jusqu'à une certaine génération
+        -- Obtenir les Identifiants des successeur d'un noeud d'une certaine génération
+        -- Paramètres :
+        --              Arbre : in T_Arbre_Binaire l'arbre sur lequel nous voulons l'information
+        --              ID : in T_Identifiant      Le noeud de départ
+        --              Generation : in Integer    La génération sur laquelle nous voulons des informations.
         -- Exception : Identifiant_Absent_Exception si l'identifiant n'est pas présent dans l'arbre.
-        function  Obt_Id_Successeur (Abr : in T_Arbre_Binaire ; ID : in T_Identifiant ; Generation : in Integer) return T_Pile;
+        -- Assure : Obt_ID_Successeuer(Arbre,ID,Generation) = Pile des Identifiant des successeurs de la génération demandée.
+        function  Obt_Id_Successeur (Arbre : in T_Arbre_Binaire ; ID : in T_Identifiant ; Generation : in Integer) return T_Pile;
 
-        -- afficher l'arbre à partir d'un certain noeud
+        -- afficher l'arbre à partir d'un certain noeud.
+        -- Paramètre :
+        --           Arbre : in T_Arbre_Binaire    L'arbre que nous voulons afficher
+        --           ID : in T_Identifiant      L'identifiant du noeud de départ
         -- Exception : Identifiant_Absent_Exception si l'identifiant n'est pas présent dans l'arbre.
-        procedure Afficher_Sous_Arbre(Abr : in T_Arbre_Binaire; ID : in T_Identifiant);
+        -- Assure :
+        --           L'arbre est affiché.
+        procedure Afficher_Sous_Arbre(Arbre : in T_Arbre_Binaire; ID : in T_Identifiant);
 
-        -- Supprimer un noeud et ses successeurs
+        -- Supprimer un noeud et ses successeurs.
+        -- Paramètres :
+        --              Arbre : in T_Arbre_Binaire L'arbre que nous voulons modifier.
+        --              ID : in T_Identifiant L'identifiant du noeud de départ.
         --Exception : Identifiant_Absent_Exception si l'identifiant n'est pas présent dans l'arbre
-        procedure Supprimer_Sous_Arbre(Abr : in T_Arbre_Binaire; ID : in T_Identifiant);
+        -- Assure :
+        --         Le noeud et ses successeurs ne sont plus présent.
+        procedure Supprimer_Sous_Arbre(Arbre : in T_Arbre_Binaire; ID : in T_Identifiant);
 
-        -- Obtenir l'ensemble des clés ayant n branches directes suivantes
+        -- Obtenir l'ensemble des clés ayant n {0,1,2} branches directes suivantes.
+        -- Paramètres :
+        --              Arbre : in T_Arbre_Binaire L'arbre sur lequel nous voulons l'information
+        --              N : in Integer  Le nombres de successeurs directs.
         -- Exception : Identifiant_Absent_Exception si l'identifiant n'est pas présent dans l'arbre
-        function Id_Possedant_N_Successeurs_Directs(Abr : in T_Arbre_Binaire; ID : in T_Identifiant; N : in Integer) return T_Pile;
+        function Id_Possedant_N_Successeurs_Directs(Abr : in T_Arbre_Binaire; N : in Integer) return T_Pile;
 
 private
 
