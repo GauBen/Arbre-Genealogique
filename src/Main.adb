64818abfd1d98b0f6caee6498edb4846b6ee0dcd
GauBen <benaim.gautier@gmail.com>
2019-11-29 11:14:54 +0100

    feat: menu_registre and menu_registre_consultation

--- src/Main.adb
+++ src/Main.adb
@@ -6,7 +6,8 @@ procedure Main is
 
    Todo_Exception : exception;
 
-   type T_Menu is (Menu_Principal, Quitter);
+   type T_Menu is
+     (Menu_Principal, Menu_Registre, Menu_Registre_Consultation, Quitter);
 
    type T_Etat is record
       Cle  : Integer;
@@ -24,32 +25,60 @@ procedure Main is
          Put (" ou ");
          Put (Nb_Choix - 0, 0);
       end Afficher_Liste_Choix;
-      Correct         : Boolean := False;
-      Premiere_Entree : Boolean := True;
+      Correct : Boolean := False;
+      -- Premiere_Entree : Boolean := True;
    begin
       while not Correct loop
          Put ("Votre choix [");
          Afficher_Liste_Choix;
-         if not Premiere_Entree then
-            Put (" / 0 pour quitter");
-         end if;
+         -- Fonctionnalité desactivée :
+         --if not Premiere_Entree then
+         --   Put (" / 0 pour quitter");
+         --end if;
          Put ("] : ");
          begin
             Get (Choix);
-            Correct := Choix in 0 .. Nb_Choix;
+            Correct := Choix in 1 .. Nb_Choix;
+            --Correct := Choix in 0 .. Nb_Choix;
          exception
             when Ada.Io_Exceptions.Data_Error =>
                Correct := False;
          end;
          Skip_Line;
          if not Correct then
-            Put_Line ("Chox incorrect.");
+            Put_Line ("Choix incorrect.");
+            New_Line;
+         end if;
+         -- Premiere_Entree := False;
+      end loop;
+
+   end Choisir;
+
+   procedure Choisir_Cle (Cle : out Integer) is
+      Correct         : Boolean := False;
+      Premiere_Entree : Boolean := True;
+   begin
+      while not Correct loop
+         Put ("Votre choix");
+         if not Premiere_Entree then
+            Put (" [0 pour retour]");
+         end if;
+         Put (" : ");
+         begin
+            Get (Cle);
+            Correct := Cle >= 0;
+         exception
+            when Ada.Io_Exceptions.Data_Error =>
+               Correct := False;
+         end;
+         Skip_Line;
+         if not Correct then
+            Put_Line ("Clé incorrecte.");
             New_Line;
          end if;
          Premiere_Entree := False;
       end loop;
-
-   end Choisir;
+   end Choisir_Cle;
 
    procedure Afficher_Menu_Principal (Etat : in out T_Etat) is
       Choix : Integer;
@@ -66,19 +95,65 @@ procedure Main is
       Choisir (3, Choix);
       case Choix is
          when 1 =>
-            raise Todo_Exception;
+            Etat.Menu := Menu_Registre;
          when 2 =>
             raise Todo_Exception;
          when others =>
             Etat.Menu := Quitter;
       end case;
+      New_Line;
    end Afficher_Menu_Principal;
+
+   procedure Afficher_Menu_Registre (Etat : in out T_Etat) is
+      Choix : Integer;
+   begin
+      Put_Line ("* Registre *");
+      New_Line;
+      Put_Line ("Menu :");
+      Put_Line ("1. Consulter une personne a partir de sa clé");
+      Put_Line ("2. Chercher une personne a partir de son nom");
+      Put_Line ("3. Ajouter une personne");
+      Put_Line ("4. Retour");
+      New_Line;
+      Choisir (4, Choix);
+      case Choix is
+         when 1 =>
+            Etat.Menu := Menu_Registre_Consultation;
+         when 2 =>
+            raise Todo_Exception;
+         when 3 =>
+            raise Todo_Exception;
+         when 4 =>
+            Etat.Menu := Menu_Principal;
+         when others =>
+            Etat.Menu := Quitter;
+      end case;
+      New_Line;
+   end Afficher_Menu_Registre;
+
+   procedure Afficher_Menu_Registre_Consultation (Etat : in out T_Etat) is
+      Cle : Integer;
+   begin
+      Put_Line ("* Consultation du registre *");
+      New_Line;
+      Put_Line ("Entrez la clé de la personne que vous voulez consulter.");
+      Choisir_Cle (Cle);
+      if Cle = 0 then
+         Etat.Menu := Menu_Registre;
+         return;
+      end if;
+      raise Todo_Exception;
+   end Afficher_Menu_Registre_Consultation;
 
    procedure Afficher_Menu (Etat : in out T_Etat) is
    begin
       case Etat.Menu is
          when Menu_Principal =>
             Afficher_Menu_Principal (Etat);
+         when Menu_Registre =>
+            Afficher_Menu_Registre (Etat);
+         when Menu_Registre_Consultation =>
+            Afficher_Menu_Registre_Consultation (Etat);
          when others =>
             Etat.Menu := Quitter;
       end case;
