e234df8f30777db998fa6582e1ccf8e3ea45cd2e
TiboSarl <thibaud.sarlat31@gmail.com>
2019-12-04 13:24:55 +0100

    fix: pretty print

--- src/test_registre_assert.adb
+++ src/test_registre_assert.adb
@@ -1,63 +1,62 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 with Registre;
 
-procedure test_registre_assert is 
+procedure test_registre_assert is
 
-    package Registre_Test is new Registre(100,Integer);
-    use Registre_Test;
+   package Registre_Test is new Registre (100, Integer);
+   use Registre_Test;
 
-    procedure Test_Initialiser is 
-        Registre : T_Registre;
-    begin
-        Initialiser(Registre);
-        pragma assert(Est_Vide(Registre));
-        Detruire(Registre);
-    end Test_Initialiser;
+   procedure Test_Initialiser is
+      Registre : T_Registre;
+   begin
+      Initialiser (Registre);
+      pragma Assert (Est_Vide (Registre));
+      Detruire (Registre);
+   end Test_Initialiser;
 
-    procedure Test_Attribuer is 
-        Registre : T_Registre;
-    begin
-        Initialiser(Registre);
-        Attribuer(Registre,1,1);
-        pragma assert(Existe(Registre,1));
-        Detruire(Registre);
-    end Test_Attribuer;
+   procedure Test_Attribuer is
+      Registre : T_Registre;
+   begin
+      Initialiser (Registre);
+      Attribuer (Registre, 1, 1);
+      pragma Assert (Existe (Registre, 1));
+      Detruire (Registre);
+   end Test_Attribuer;
 
-    procedure Test_Acceder is 
-        Registre : T_Registre;
-    begin  
-        Initialiser(Registre);
-        Attribuer(Registre,1,1);
-        pragma assert(Acceder(Registre,1)=1);
-        Detruire(Registre);
-    end Test_Acceder;
+   procedure Test_Acceder is
+      Registre : T_Registre;
+   begin
+      Initialiser (Registre);
+      Attribuer (Registre, 1, 1);
+      pragma Assert (Acceder (Registre, 1) = 1);
+      Detruire (Registre);
+   end Test_Acceder;
 
-    procedure Test_Supprimer is 
-        Registre : T_Registre;
-    begin
-        Initialiser(Registre);
-        Attribuer(Registre,1,2);
-        Supprimer(Registre,1);
-        pragma assert(not Existe(Registre,1));
-        Detruire(Registre);
-    end Test_Supprimer;
+   procedure Test_Supprimer is
+      Registre : T_Registre;
+   begin
+      Initialiser (Registre);
+      Attribuer (Registre, 1, 2);
+      Supprimer (Registre, 1);
+      pragma Assert (not Existe (Registre, 1));
+      Detruire (Registre);
+   end Test_Supprimer;
 
-    procedure Test_Detruire is 
-        Registre : T_Registre;
-    begin
-        Initialiser(Registre);
-        Attribuer(Registre,1,2);
-        Attribuer(Registre,3,4);
-        Attribuer(Registre,2,5);
-        Detruire(Registre);
-        pragma assert(Est_Vide(Registre));
-    end Test_Detruire;
+   procedure Test_Detruire is
+      Registre : T_Registre;
+   begin
+      Initialiser (Registre);
+      Attribuer (Registre, 1, 2);
+      Attribuer (Registre, 3, 4);
+      Attribuer (Registre, 2, 5);
+      Detruire (Registre);
+      pragma Assert (Est_Vide (Registre));
+   end Test_Detruire;
 begin
-    Test_Initialiser;
-    Test_Acceder;
-    Test_Attribuer;
-    Test_Supprimer;
-    Test_Detruire;
-    Put_Line("Le module est fonctionnel");
+   Test_Initialiser;
+   Test_Acceder;
+   Test_Attribuer;
+   Test_Supprimer;
+   Test_Detruire;
+   Put_Line ("Le module est fonctionnel");
 end test_registre_assert;
